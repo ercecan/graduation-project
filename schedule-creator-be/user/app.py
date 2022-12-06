88636d2e5a357f098aeec9e51535b4938edc7b8f
Erce Can Bektüre <canerce99@gmail.com>
2022-12-06 16:46:59 +0300

    ADD: added cors stettings

--- schedule-creator-be/user/app.py
+++ schedule-creator-be/user/app.py
@@ -4,8 +4,9 @@ from starlette.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import logging
 import router
-import config
 from config import CONFIG
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # if not local environment hide api docs url
@@ -14,6 +15,24 @@ if CONFIG.env != 'local':
 else:
     app = FastAPI()
 
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["*"]  # edit later
+)
+
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(router.router)
 
-userDB = {'users': []}
+
