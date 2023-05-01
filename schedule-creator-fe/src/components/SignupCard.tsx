b14942d613a84a243c4fb56f91cc9ed455db5ba0
Ömer <oomeroozkan@gmail.com>
2023-05-01 19:15:46 +0300

    signup card connect with be

--- schedule-creator-fe/src/components/SignupCard.tsx
+++ schedule-creator-fe/src/components/SignupCard.tsx
@@ -1,39 +1,40 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Button, Checkbox, Form, Input } from "antd";
+import { Button, Col, Form, Input, Row, Select } from "antd";
 import { useNavigate } from "react-router-dom";
 import logo from "../icon.png";
 import axios from "axios";
 
 const StyledErrorMessage = styled.div``;
 const StyledImage = styled.img`
-  margin-left: 120px;
+  margin-left: 50px;
   padding-bottom: 30px;
   width: 150px;
 `;
 const StyledContainer = styled.div`
-  width: 500px;
+  width: 1000px;
   position: absolute;
-  left: 28%;
-  top: 25%;
+  left: 10%;
+  overflow-y: auto;
+
+  .ant-row {
+    gap: 16px;
+  }
 `;
 
 const SignupCard = (): JSX.Element => {
   const [valid, setValid] = useState(true);
+  const [counter, setCounter] = useState(0);
   const navigate = useNavigate();
-  const url = "http://localhost:8000/api/user/login";
+  const url = "http://localhost:8000/api/student/register";
 
   const onFinish = async (values: any) => {
     console.log(values);
-    // email: str
-    // password: str
-    // name: str
-    // student_id: str
     const response = await axios.post(url, values);
     console.log(response);
     if (response.status === 200) {
       setValid(true);
-      navigate("/panel");
+      navigate("/home");
     } else {
       setValid(false);
     }
@@ -58,38 +59,140 @@ const SignupCard = (): JSX.Element => {
         onFinishFailed={onFinishFailed}
         autoComplete="off"
       >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: "Please input your email!" }]}
-        >
-          <Input />
-        </Form.Item>
+        <Row>
+          <Col span={10}>
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: "Please input your name!" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Surname"
+              name="surname"
+              rules={[{ required: true, message: "Please input your name!" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Student ID"
+              name="student_id"
+              rules={[
+                { required: true, message: "Please input your student id!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-        <Form.Item
-          label="Student ID"
-          name="id"
-          rules={[{ required: true, message: "Please input your student id!" }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Name"
-          name="name"
-          rules={[{ required: true, message: "Please input your name!" }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
+            <Form.Item
+              label="School"
+              name="school"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Year"
+              name="year"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="GPA"
+              name="gpa"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={10}>
+            <Form.List name="major">
+              {(fields, { add, remove }) => (
+                <>
+                  {fields.map((field, index) => (
+                    <div key={field.key}>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, "name"]}
+                        rules={[
+                          { required: true, message: "Please enter a name" },
+                        ]}
+                      >
+                        <Input placeholder="Name" />
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, "code"]}
+                        rules={[
+                          { required: true, message: "Please enter a code" },
+                        ]}
+                      >
+                        <Input placeholder="Code" />
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        name={[field.name, "language"]}
+                        rules={[
+                          {
+                            required: true,
+                            message: "Please enter a language",
+                          },
+                        ]}
+                      >
+                        <Input placeholder="Language" />
+                      </Form.Item>
+                      <Button
+                        style={{ marginRight: "10px" }}
+                        type="dashed"
+                        onClick={() => {
+                          remove(field.name);
+                          setCounter(counter - 1);
+                        }}
+                      >
+                        Remove
+                      </Button>
+                    </div>
+                  ))}
+                  {counter < 2 && (
+                    <Button
+                      type="dashed"
+                      onClick={() => {
+                        add();
+                        setCounter(counter + 1);
+                      }}
+                    >
+                      Add major
+                    </Button>
+                  )}
+                </>
+              )}
+            </Form.List>
+          </Col>
+        </Row>
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
           <Button type="primary" htmlType="submit">
             Submit
