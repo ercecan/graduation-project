1ce5260fbf4dafbffe875776ab94ffe43a9527f5
barisemremise <barisemremise@hotmail.com>
2023-05-17 22:34:31 +0300

    fix: opened_course updated

--- user_service/utils/opened_course_utils.py
+++ user_service/utils/opened_course_utils.py
@@ -5,7 +5,7 @@ from models.time import Term, TimeSlot
 from models.classroom import Classroom
 
 def create_course_from_dto(opened_course: OpenedCourseDto) -> Course:
-    course = Course(name=opened_course.name, code=opened_course.code, crn=opened_course.crn, ects=opened_course.ects,
+    course = Course(name=opened_course.name, code=opened_course.code, ects=opened_course.ects,
                     credits=opened_course.credits, language=opened_course.language,
                     major_restrictions=opened_course.major_restrictions, prereqs=opened_course.prereqs,
                     year_restrictions=opened_course.year_restrictions, description=opened_course.description,
@@ -23,5 +23,6 @@ def create_opened_course_from_dto(opened_course: OpenedCourseDto, course_id: str
     if opened_course.classroom:
         classroom = Classroom(room=opened_course.classroom['room'], building=opened_course.classroom['building'])
     opened_course = OpenedCourse(course_id=course_id, term=term, time_slot=time_slot, classroom=classroom,
-                                 capacity=opened_course.capacity, teaching_method=opened_course.teaching_method)
+                                 capacity=opened_course.capacity, teaching_method=opened_course.teaching_method,
+                                 crn=opened_course.crn)
     return opened_course
